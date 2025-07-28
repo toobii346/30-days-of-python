98fdce68d0479f75e190436dab77ce8f018fe4c5
toobii346 <tobijoeleboka@gmail.com>
2025-07-28 17:45:49 +0100

    Day 1 finished

--- day-1/app.py
+++ day-1/app.py
@@ -1,1 +1,68 @@
-print('Hello world')
+# Exercise 1
+# Python version has been checked
+
+# Exercise 2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+# Exercise 3
+
+print("Tobi")
+print("Eboka")
+print("Ireland")
+print("I am enjoying 30 days of python")
+
+# Exercise 4
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(["Java", "Python", "C_plus"]))
+print(type(Tobi))
+print(type(Eboka))
+
+#Level 3:
+# Exercise 1
+
+int_num = 25
+float_num = 9.81
+complex_num = 2 + 3j
+
+name = "Tobi"
+
+is_coding_fun = True
+is_day_over = False
+
+fruits = ["Apple", "Banana", "Cherry"]
+
+dimensions = (1920, 1080)
+
+unique_numbers = {1, 2, 3, 4}
+
+print(type(int_num))
+print(type(float_num))
+print(type(complex_num))
+print(type(name))
+print(type(is_coding_fun))
+print(type(fruits))
+print(type(dimensions))
+print(type(unique_numbers))
+
+# Exercise 2
+
+import math
+
+x1, y1 = 2, 3
+x2, y2 = 10, 8
+
+distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+print("Euclidean distance:", distance)
+
+
+
+
--- day-1/reflection.md
+++ day-1/reflection.md
@@ -1,4 +1,16 @@
 # Day 1 reflection
 
-- Today I learnt what Python is all about
-- hdhdhdh
+Today I learned how to use the Python shell and wrote my first Python script.
+
+# I learned to:
+- Install Python and VS Code
+- Run Python in the terminal and inside a file
+- Basic Python syntax and indentation rules
+- What comments are and how to write them
+- The most common Python data types:
+  - int, float, complex
+  - string
+  - boolean
+  - list, tuple, set, dictionary
+- Simple math operations and how Python handles them
+- How to calculate the Euclidean distance between two points using math
